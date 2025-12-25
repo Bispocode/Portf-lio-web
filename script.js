3bbfbc5fc1a63f8d280d3febb2d5dc9d7b4b8b39
Bispocode <lbispodeandrade@gmail.com>
2025-12-25 13:04:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const lista = document.querySelector('.lista-projetos');
+
+if (lista) {
+    lista.addEventListener('wheel', (e) => {
+        if (lista.scrollWidth > lista.clientWidth) {
+            e.preventDefault();
+            lista.scrollLeft += e.deltaY;
+        }
+    }, { passive: false });
+}
+console.log('JS carregado');
+
 
